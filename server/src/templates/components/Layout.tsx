4a9193aa74e94f76af7de8d9477df35482146ea6
Baozier <byn9826@gmail.com>
2024-07-29 22:25:38 -0400

    Fix email padding for better mobile display (#56)

--- server/src/templates/components/Layout.tsx
+++ server/src/templates/components/Layout.tsx
@@ -17,10 +17,10 @@ const Layout = ({
   <body
     style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.6; color: #333333; margin: 0; padding: 0;"
   >
-    <div style="max-width: 600px; margin: 0 auto; padding: 80px; background-color: #f7f7f7;">
+    <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f7f7f7;">
       <table width="100%" cellpadding="0" cellspacing="0" border="0">
         <tr>
-            <td align="center" style="padding: 80px 0;">
+            <td align="center">
                 <table
                   width="600"
                   cellpadding="0"
