bc456bec2329e0fec42275fac69ffe57b9748c9e
byn9826 <byn9826@gmail.com>
2024-10-16 20:49:25 -0400

    Add basic tests for admin panel logs page

---
+++ admin-panel/tests/logMock.ts
@@ -0,0 +1,66 @@
+export const emailLogs = [
+  {
+    id: 1,
+    success: true,
+    receiver: 'test1@email.com',
+    response: 'response 1',
+    content: 'content 1',
+    createdAt: '2024-08-07 20:45:27',
+    updatedAt: '2024-08-07 20:45:27',
+    deletedAt: null,
+  },
+  {
+    id: 2,
+    success: true,
+    receiver: 'test2@email.com',
+    response: 'response 2',
+    content: 'content 2',
+    createdAt: '2024-08-07 20:45:27',
+    updatedAt: '2024-08-07 20:45:27',
+    deletedAt: null,
+  },
+]
+
+export const smsLogs = [
+  {
+    id: 1,
+    success: true,
+    receiver: '+16471231111',
+    response: 'response 1',
+    content: 'content 1',
+    createdAt: '2024-08-07 20:45:27',
+    updatedAt: '2024-08-07 20:45:27',
+    deletedAt: null,
+  },
+  {
+    id: 2,
+    success: true,
+    receiver: '+16471231112',
+    response: 'response 2',
+    content: 'content 2',
+    createdAt: '2024-08-07 20:45:27',
+    updatedAt: '2024-08-07 20:45:27',
+    deletedAt: null,
+  },
+]
+
+export const signInLogs = [
+  {
+    id: 1,
+    userId: 1,
+    ip: '1-1-1-1',
+    detail: 'test',
+    createdAt: '2024-08-07 20:45:27',
+    updatedAt: '2024-08-07 20:45:27',
+    deletedAt: null,
+  },
+  {
+    id: 2,
+    userId: 2,
+    ip: '1-1-1-2',
+    detail: 'test 2',
+    createdAt: '2024-08-07 20:45:27',
+    updatedAt: '2024-08-07 20:45:27',
+    deletedAt: null,
+  },
+]
