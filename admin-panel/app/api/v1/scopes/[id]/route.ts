ae7e54889cc65d05bd4830d1caa525f3b4e43435
byn9826 <byn9826@gmail.com>
2024-12-16 22:23:33 -0500

    save

--- admin-panel/app/api/v1/scopes/[id]/route.ts
+++ admin-panel/app/api/v1/scopes/[id]/route.ts
@@ -22,12 +22,7 @@ export async function PUT (
   request: Request, context: { params: Params },
 ) {
   const id = context.params.id
-  console.log(context.params.id)
-  console.log(context)
-  console.log(request)
-
   const reqBody = await request.json()
-  console.log(reqBody)
   if (!reqBody) return throwForbiddenError()
 
   return sendS2SRequest({
