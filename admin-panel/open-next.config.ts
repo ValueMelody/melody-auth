f5ae546c4e8f635f7e8607cb4f896af8a817dda2
Baozier <byn9826@gmail.com>
2025-03-28 22:48:54 -0400

    update config

--- admin-panel/open-next.config.ts
+++ admin-panel/open-next.config.ts
@@ -11,15 +11,6 @@ const config: OpenNextConfig = {
       queue: 'dummy',
     },
   },
-
-  middleware: {
-    external: true,
-    override: {
-      wrapper: 'cloudflare-edge',
-      converter: 'edge',
-      proxyExternalRequest: 'fetch',
-    },
-  },
 }
 
 export default config
