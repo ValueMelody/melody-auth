9f6a42f318b129175d556165c6860a7a0c1f9b0d
byn9826 <byn9826@gmail.com>
2026-01-06 19:19:29 -0500

    update config

--- admin-panel/open-next.config.ts
+++ admin-panel/open-next.config.ts
@@ -11,6 +11,18 @@ const config: OpenNextConfig = {
       queue: 'dummy',
     },
   },
+  edgeExternals: ['node:crypto'],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: 'cloudflare-edge',
+      converter: 'edge',
+      proxyExternalRequest: 'fetch',
+      incrementalCache: 'dummy',
+      tagCache: 'dummy',
+      queue: 'dummy',
+    },
+  },
 }
 
 export default config
